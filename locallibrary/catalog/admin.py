1749cd3210efa36ffff9e7ff5808236df39655bc
Steve Baker <stevebaker01@gmail.com>
2017-03-19 20:17:27 -0700

    admin site stuff

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,17 +4,32 @@ from .models import Author, Genre, Book, BookInstance
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name',
+                    'first_name',
+                    'date_of_birth',
+                    'date_of_death');
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {'fields': ('book', 'imprint', 'id')}),
+        ('Availability', {'fields': ('status', 'due_back')})
+    )
+
 
 
 # admin.site.register(Book)
